d6f1f4621d66b12ba00ae92de4f8087a152f6a00
mosesfawole <mosesfawole2001@gmail.com>
2023-10-19 14:41:51 +0100

    initial contact page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import HeroImg from "public/hero.png";
 import { Button } from "@/components";
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <h1 className={styles.title}>
-          Better design for your digital products
+          Better design for your digital products.
         </h1>
         <p className={styles.desc}>
-          Turning your Idea into Reality. We bring together the teams from
-          global tech industry
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
         </p>
         <Button url="/portfolio" text="See Our Works" />
-
-        <button className={styles.button}></button>
       </div>
-      <div className={styles.item}></div>
-      <Image src={HeroImg} alt="banner img" className={styles.img} />
+      <div className={styles.item}>
+        <Image src={HeroImg} alt="" className={styles.img} />
+      </div>
     </div>
   );
 }
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./button.module.css";
 import Link from "next/link";
-const Button = () => {
+const Button = ({ url, text }) => {
   return (
     <Link href={url}>
       <button className={styles.container}> {text}</button>
