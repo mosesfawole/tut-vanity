be8bb0da063ef1ccbc0af8f34f06900543ee86ba
mosesfawole <mosesfawole2001@gmail.com>
2023-10-21 10:31:30 +0100

    add user and psot model

---
+++ src/models/post.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", postSchema);
