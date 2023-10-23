e2b9ec0bc98bf0cfb56dfa960b95e8aa3be29360
mosesfawole <mosesfawole2001@gmail.com>
2023-10-23 12:29:51 +0100

    handle register auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,43 @@
+import connect from "@/utils/db";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      async authorize(credentials) {
+        await connect();
+
+        try {
+          const user = User.findOne({ email: credentials.email });
+
+          if (user) {
+            // check passwrd
+            const checkPassword = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+
+            if (checkPassword) {
+              return user;
+            } else {
+              ("Wrong Credentials");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
     }),
   ],
 });
--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -6,6 +6,28 @@ import { signIn } from "next-auth/react";
 const Login = () => {
   return (
     <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          placeholder="Username"
+          required
+          className={styles.input}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+        {error && "Something went wrong!"}
+      </form>
       <button onClick={() => signIn("google")}>Login with Google</button>
     </div>
   );
