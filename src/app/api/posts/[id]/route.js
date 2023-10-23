e6722ecc80a1416c592d8444758d0ee15fcff6e7
mosesfawole <mosesfawole2001@gmail.com>
2023-10-23 13:22:06 +0100

    add post and delete function

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -15,3 +15,19 @@ export const GET = async (request, { params }) => {
     return new NextResponse(error, { status: 500 });
   }
 };
+
+export const DELETE = async (request, { params }) => {
+  const { id } = params;
+
+  try {
+    await connect();
+
+    await Post.findByIdAndDelete(id);
+
+    return new NextResponse(JSON.stringify("Post has been deleted"), {
+      status: 200,
+    });
+  } catch (error) {
+    return new NextResponse(error, { status: 500 });
+  }
+};
